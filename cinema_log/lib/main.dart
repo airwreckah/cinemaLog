041c188f725b6284bb97d7fc167300e1d6dd6323
Amabilia <abbyrussell1226@gmail.com>
2026-03-08 14:48:29 -0500

    Built up the welcome screen

--- cinema_log/lib/main.dart
+++ cinema_log/lib/main.dart
@@ -1,3 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:cinema_log/screens/welcome.dart';
 
- 
+void main(){
+  runApp(CinemaLog());
+} 
+
+class CinemaLog extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cinema Log',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF615FFF),
+          brightness: Brightness.dark
+        ),
+      ),
+      home: const Welcome(),
+    );
+    //throw UnimplementedError();
+  }
+}
---
+++ cinema_log/lib/screens/welcome.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class Welcome extends StatefulWidget{
+  const Welcome({super.key});
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<Welcome>{
+  //controller for text
+  final TextEditingController _controller = TextEditingController();
+    
+  @override 
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CinemaLog'), //header title
+      ),
+        body: Column( 
+          children: <Widget>[
+            Text('Welcome!\nPlease sign up or login to start tracking what you watch'),
+            ElevatedButton(
+              onPressed: null,
+              child: const Text('Sign up'),
+              ),
+            ElevatedButton(onPressed: null, child: const Text('Log in')),
+            Container(
+              height: 25,
+             // color: Color(0xFF030712),
+              child: const Center(child: Text( 'Popular', textAlign: TextAlign.justify)),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 5),
+              height: 200,
+              child: ScrollConfiguration(behavior: const MaterialScrollBehavior().copyWith(dragDevices: {...PointerDeviceKind.values}), 
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children:[
+                  for(final color in Colors.primaries)
+                    Container(width: 160, color:color),
+                        ],
+                      )
+                    ),
+                  ),
+          ]
+        ),
+         bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),//Home tab
+              label: 'Home'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),//Searflch tab
+              label: 'Search'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),//List tab
+              label: 'Lists'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),//Profile tab
+              label: 'Profile',
+              ),
+          ],
+        ), //for navigation at bottom of screen
+      );
+    }
+}
+
+
+
