d05df3b981ff50c46e3095597836ccb64b74cc9e
airwreckah <erikadelgado10@yahoo.com>
2026-03-22 23:26:35 -0500

    Refactor Controller to integrate authentication and media methods
    Refactor Controller class to use AuthService and TrackerManager. Added methods for authentication and media management.

--- cinema_log/lib/services/controller.dart
+++ cinema_log/lib/services/controller.dart
@@ -1,20 +1,84 @@
-import 'package:movie_tracker/movie_tracker.dart' as movie_tracker;
+import '../models/media.dart';
+import 'auth_service.dart';
+import 'tracker_manager.dart';
 
 class Controller {
-  TrackerManager trackerManager = TrackerManager();
+  final AuthService _authService = AuthService();
+  final TrackerManager _trackerManager = TrackerManager();
 
-  void addMovie(String name) {
-    trackerManager.addMovie(name);
+  // AUTHENTICATION
+  Future<bool> signIn(String email, String password) async {
+    try {
+      await _authService.signIn(email, password);
+      return true;
+    } catch (e) {
+      return false;
+    }
   }
-}
 
-void main(List<String> arguments) {
-  Controller controller = Controller();
+  Future<void> signOut() async {
+    await _authService.signOut();
+  }
 
-  print(controller.addMovie("The Matrix"));
-  print(controller.addMovie("Fight Club"));
+  // TRACKER
 
-  print(controller.searchMovie("The Matrix"));
+  void addToWatchList(Media media) {
+    _trackerManager.addToWatchList(media);
+  }
 
-  print(controller.listMovies);
+  void removeFromWatchList(Media media) {
+    _trackerManager.removeFromWatchList(media);
+  }
+
+  void markAsWatched(Media media) {
+    _trackerManager.markAsWatched(media);
+  }
+
+  void markAsUnwatched(Media media) {
+    _trackerManager.markAsUnwatched(media);
+  }
+
+  List<Media> getWatchList() {
+    return _trackerManager.getWatchList();
+  }
+
+  List<Media> getWatchHistory() {
+    return _trackerManager.getWatchHistory();
+  }
+
+  int getTotalWatched() {
+    return _trackerManager.getTotalWatched();
+  }
+
+  int getWatchListCount() {
+    return _trackerManager.getWatchListCount();
+  }
+
+  double getCompletionRate() {
+    return _trackerManager.getCompletionRate();
+  }
+
+  Media? getMediaById(String id) {
+    return _trackerManager.getMediaById(id);
+  }
+
+  void removeFromWatchListById(String id) {
+    _trackerManager.removeFromWatchListById(id);
+  }
+
+  void removeFromHistory(Media media) {
+    _trackerManager.removeFromHistory(media);
+  }
+
+  void removeFromHistoryById(String id) {
+    _trackerManager.removeFromHistoryById(id);
+  }
+
+  void clearHistory() {
+    _trackerManager.clearHistory();
+  }
+
+  void resetAll() {
+    _trackerManager.resetAll();
+  }
 }
