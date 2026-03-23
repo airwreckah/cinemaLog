d7b86bcc44205d0063e118c83d0f411061e17cfd
airwreckah <erikadelgado10@yahoo.com>
2026-03-22 20:40:52 -0500

    Sign In Autentication

---
+++ cinema_log/lib/services/auth_service.dart
@@ -0,0 +1,15 @@
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<User?> signIn(String email, String password) async {
+    final result = await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return result.user;
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+}
